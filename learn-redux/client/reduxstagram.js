cb68118fb073e3d6d415259a33bf90c946c2a9df
Allison Lewis <allison.lewis@rallyhealth.com>
2021-04-03 18:40:53 -0500

    Access Dispatch and State with Redux connect

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -3,7 +3,7 @@ import { render } from 'react-dom';
 // Import CSS
 import css from './styles/style.styl';
 // Import components
-import Main from './components/Main';
+import App from './components/App';
 import Single from './components/Single';
 import PhotoGrid from './components/PhotoGrid';
 // Import React Router dependencies
@@ -14,7 +14,7 @@ import store, { history } from './store';
 const router = (
   <Provider store={store}>
     <Router history={history}>
-      <Route path="/" component={Main}>
+      <Route path="/" component={App}>
         <IndexRoute component={PhotoGrid}></IndexRoute>
         <Route path="/view/:postId" component={Single}></Route>
       </Route>
