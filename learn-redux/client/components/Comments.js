4c744619ee8e869a87341910a2a727b80fe95df0
Allison Lewis <allison.lewis@rallyhealth.com>
2021-04-03 19:32:14 -0500

    Display the single photo component

---
+++ learn-redux/client/components/Comments.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Comments = React.createClass({
+  render() {
+    return (
+      <div className="comments">
+        I'm the comments
+      </div>
+    )
+  }
+});
+
+export default Comments;
