527f6f278175ea430b662aefeada4807bd2a78d7
Allison Lewis <allison.lewis@rallyhealth.com>
2021-04-03 19:00:37 -0500

    Create Photo and display redux state within component

--- learn-redux/client/components/PhotoGrid.js
+++ learn-redux/client/components/PhotoGrid.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Photo from './Photo';
 
 const PhotoGrid = React.createClass({
   render() {
+    // {JSON.stringify(this.props.posts, null, ' ')}
     return (
       <div className="photo-grid">
-        I'm the photo grid
+        {this.props.posts.map((post, i) => <Photo {...this.props} key={i} i={i} post={post} />)}
       </div>
     )
   }
