fd70cb5da7d7fb905951f9139982e63eb5bd7502
Allison Lewis <allison.lewis@rallyhealth.com>
2021-04-05 14:32:05 -0500

    Update reducer composition for removing comments

--- learn-redux/client/reducers/comments.js
+++ learn-redux/client/reducers/comments.js
@@ -7,11 +7,16 @@ function postComments(state = [], action) {
         text: action.comment
       }];
     case 'REMOVE_COMMENT':
-      return state;
+      return [
+        // from the start to the one we want to delete
+        ...state.slice(0, action.i),
+        // after the deleted one, to the end
+        ...state.slice(action.i + 1)
+      ];
+      // return state;
     default:
       return state;
   }
-  return state;
 }
 
 function comments(state = [], action) {
